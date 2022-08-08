664fee67f0449b7b72a6b48d513756f14d235e67
Chalfe <Kewin.paepke@gmail.com>
2022-08-08 15:32:24 +0200

    Set Up scene, and did some movement

---
+++ Toadder/Assets/Scripts/Toadder.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toadder : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Move(Vector3.up);
+        }
+
+       else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Move(Vector3.down);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move(Vector3.left);
+        }
+
+       else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move(Vector3.right);
+        }
+
+        void Move(Vector3 direction)
+        {
+            transform.position += direction;
+        }
+    }
+}
