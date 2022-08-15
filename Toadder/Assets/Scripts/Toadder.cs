b082b6d885251f53a4b0c2c91be88c6e2de26bb1
Chalfe <Kewin.paepke@gmail.com>
2022-08-15 13:11:03 +0200

    Toadmation

--- Toadder/Assets/Scripts/Toadder.cs
+++ Toadder/Assets/Scripts/Toadder.cs
@@ -4,31 +4,65 @@ using UnityEngine;
 
 public class Toadder : MonoBehaviour
 {
+    private SpriteRenderer SpriteRenderer;
+    public Sprite Idle;
+    public Sprite LeapSprite;
+
+    private void Awake()
+    {
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
             Move(Vector3.up);
         }
 
        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
+            transform.rotation = Quaternion.Euler(0, 0, 180);
             Move(Vector3.down);
         }
 
         else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            transform.rotation = Quaternion.Euler(0, 0, 90);
             Move(Vector3.left);
         }
 
        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
+            transform.rotation = Quaternion.Euler(0, 0, 270);
             Move(Vector3.right);
         }
 
-        void Move(Vector3 direction)
+
+
+       
+    }
+    void Move(Vector3 direction)
+    {
+            Vector3 destination = transform.position + direction;
+            StartCoroutine((Leap(destination)));
+    }
+    private IEnumerator Leap(Vector3 destination)
+    {
+        Vector3 StartPosition = transform.position;
+        float elapsed = 0f;
+        float duration = 0.125f;
+
+        SpriteRenderer.sprite = LeapSprite;
+
+        while(elapsed < duration)
         {
-            transform.position += direction;
+            float t = elapsed / duration;
+            transform.position = Vector3.Lerp(StartPosition, destination, t);
+            elapsed += Time.deltaTime;
+            yield return null;
         }
+        transform.position = destination;
+        SpriteRenderer.sprite = Idle;
     }
 }
