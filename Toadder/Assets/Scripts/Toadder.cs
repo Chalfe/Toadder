44a6a472943cf5126075c95108d2cc0dad8ce0cd
Chalfe <Kewin.paepke@gmail.com>
2022-08-15 15:01:52 +0200

    More collision

--- Toadder/Assets/Scripts/Toadder.cs
+++ Toadder/Assets/Scripts/Toadder.cs
@@ -7,6 +7,7 @@ public class Toadder : MonoBehaviour
     private SpriteRenderer SpriteRenderer;
     public Sprite Idle;
     public Sprite LeapSprite;
+    public Sprite Dead;
 
     private void Awake()
     {
@@ -44,8 +45,41 @@ public class Toadder : MonoBehaviour
     }
     void Move(Vector3 direction)
     {
+<<<<<<< Updated upstream
             Vector3 destination = transform.position + direction;
             StartCoroutine((Leap(destination)));
+=======
+        Vector3 destination = transform.position + direction;
+
+        Collider2D barrier = Physics2D.OverlapBox(destination, Vector2.zero, 0f, LayerMask.GetMask("Barrier"));
+        Collider2D platform = Physics2D.OverlapBox(destination, Vector2.zero, 0f, LayerMask.GetMask("Platform"));
+        Collider2D obstacle = Physics2D.OverlapBox(destination, Vector2.zero, 0f, LayerMask.GetMask("Obstacle"));
+
+        if (barrier != null)
+        {
+            return;
+        }
+
+        if (platform != null)
+        {
+            transform.SetParent(platform.transform);
+        }
+        else
+        {
+            transform.SetParent(null);
+        }
+
+        if(obstacle != null && platform == null)
+        {
+            transform.position = destination;
+            Death();
+        }
+        else
+        {
+            StartCoroutine((Leap(destination)));
+        }
+
+>>>>>>> Stashed changes
     }
     private IEnumerator Leap(Vector3 destination)
     {
@@ -64,5 +98,20 @@ public class Toadder : MonoBehaviour
         }
         transform.position = destination;
         SpriteRenderer.sprite = Idle;
+    }
+
+    private void Death()
+    {
+        transform.rotation = Quaternion.identity;
+        SpriteRenderer.sprite = Dead;
+        enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (enabled && other.gameObject.layer == LayerMask.NameToLayer("Obstacle") && transform.parent == null)
+        {
+            Death();
+        }
     }
 }
